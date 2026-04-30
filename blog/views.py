b795cbc63fd610d0661fd2aebfbbd8c41bd6696e
djumanov <djumanovdev@gmail.com>
2026-04-30 17:26:16 +0500

    teplates config

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def home_page(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='home.html')
+    
